0f8d83fa92cc227365261a4cdaddcb230a6494a4
Roberto Alonso <180604094+Roberto-Mosquera@users.noreply.github.com>
2025-11-14 22:41:50 -0500

    Inicializacion de estructura de proyecto

---
+++ VotaClaro/src/main/java/org/app/votaclaro/VotaClaroApplication.java
@@ -0,0 +1,13 @@
+package org.app.votaclaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotaClaroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotaClaroApplication.class, args);
+    }
+
+}
---
+++ VotaClaro/src/test/java/org/app/votaclaro/VotaClaroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.votaclaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotaClaroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
